d501098aa6b1aaf4f9dcd14566fe9c4fa6618deb
Chip Carnes and Sami Al-Jamal <chip@>
2016-05-31 14:50:18 -0700

    created toggle functions

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  $("body").addClass("gray-background");
+  $(".clickable").click(function(){
+    $("#var-def").toggle();
+    $("#var-term").toggle();
+  });
+  $(".clickable1").click(function(){
+    $("#func-def").toggle();
+    $("#func-term").toggle();
+
+
+  });
+  $(".clickable2").click(function(){
+    $("#func-def1").toggle();
+    $("#func-term1").toggle();
+
+
+  });
+  $(".clickable3").click(function(){
+    $("#func-def2").toggle();
+    $("#func-term2").toggle();
+
+  });
+  $(".clickable4").click(function(){
+    $("#func-def3").toggle();
+    $("#func-term3").toggle();
+
+
+  });
+  $(".clickable5").click(function(){
+    $("#func-def4").toggle();
+    $("#func-term4").toggle();
+
+
+  });
+  $(".clickable6").click(function(){
+    $("#func-def5").toggle();
+    $("#func-term5").toggle();
+
+
+  });
+  $(".clickable7").click(function(){
+    $("#func-def6").toggle();
+    $("#func-term6").toggle();
+
+
+  });
+
+});
